ce02eecce30631a3f5a8ba05e948a67ec3c4acc7
TurboPug2101 <ishank.219310145@muj.manipal.edu>
2023-06-04 01:08:39 +0530

    first

---
+++ src/components/Carousal.js
@@ -0,0 +1,62 @@
+// import React from 'react'
+
+// export default function Carousal() {
+//   return (
+// //     <div>
+// //         <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+// //   <div className="carousel-indicators">
+// //     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+// //     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+// //     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+// //   </div>
+// //   <div className="carousel-inner">
+    
+
+// //     <div className="carousel-item active" data-bs-interval="10000" id='carousal'>
+// //     <img src="https://source.unsplash.com/random/60×40/?burger" className="d-block w-100" alt="..."/>
+// //       <div className="carousel-caption d-none d-md-block">
+// //       <form className="d-flex">
+// //         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+// //         <button className="btn btn-outline-success" type="submit">Search</button>
+// //       </form>
+// //       </div>
+// //     </div>
+// //     <div className="carousel-item" id='carousal' data-bs-interval="200 0"  >
+// //     <img src="https://source.unsplash.com/random/60×40/?pizza" className="d-block w-100" alt="..."/>
+// //       <div className="carousel-caption d-none d-md-block">
+// //       <form className="d-flex">
+// //         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+// //         <button className="btn btn-outline-success" type="submit">Search</button>
+// //       </form>
+// //       </div>
+// //     </div>
+// //     <div className="carousel-item" id='carousal'>
+// //     <img src="https://source.unsplash.com/random/60×40/?pastry" className="d-block w-100" alt="..."/>
+// //       <div className="carousel-caption d-none d-md-block">
+    
+// //       <form className="d-flex">
+// //         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+// //         <button className="btn btn-outline-success" type="submit">Search</button>
+// //       </form>
+      
+// //       </div>
+     
+// //     </div>
+    
+// //   </div>
+// //   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+// //     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+// //     <span className="visually-hidden">Previous</span>
+// //   </button>
+// //   <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+// //     <span className="carousel-control-next-icon" aria-hidden="true"></span>
+// //     <span className="visually-hidden">Next</span>
+// //   </button>
+// // </div>
+// //     </div>
+      
+//   )
+// }
+
+
+
